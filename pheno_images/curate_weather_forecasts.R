198fce1cc642eb2a214c5f122262d647b83ac0f0
Kristina Riemer <kristina.riemer@weecology.org>
2021-04-12 12:56:12 -0700

    Add date time column to forecasted weather data

--- pheno_images/curate_weather_forecasts.R
+++ pheno_images/curate_weather_forecasts.R
@@ -1,7 +1,18 @@
+###########Read in libraries###########
 # devtools::install_github('eco4cast/neon4cast')
 # install.packages('readr')
 library(neon4cast)
-pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ)"
+library(readr)
+library(dplyr)
+
+###########Download weather data###########
+pheno_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ")
 download_noaa(pheno_sites)
 noaa_fc <- stack_noaa()
-readr::write_csv(noaa_fc, file = paste0('pheno_images/NOAA_GEFS_35d_', Sys.Date(), '.csv'))
+
+###########Clean up weather data###########
+forecast_weather <- noaa_fc %>% 
+  mutate(date_time = as.Date(substr(startDate, 1, 10)) + lubridate::hours(time))
+
+###########Save weather csv###########
+write_csv(forecast_weather, file = paste0('pheno_images/NOAA_GEFS_35d_', Sys.Date(), '.csv'))
